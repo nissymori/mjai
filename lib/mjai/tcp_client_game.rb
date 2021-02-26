38901d5cf875ea24058a1f564f0f2adca07b0a2c
nissymori <kyoudai.630@gmail.com>
2021-02-26 15:44:39 +0900

    undo tcpclientgame

--- lib/mjai/tcp_client_game.rb
+++ lib/mjai/tcp_client_game.rb
@@ -20,46 +20,39 @@ module Mjai
 
         def play()
           uri = URI.parse(@params[:url])
-          $stderr.puts(uri.host)
-          $stderr.puts(uri.port)
-          begin  #　例外処理
-            uri = URI.parse(@params[:url])
-            TCPSocket.open(uri.host, uri.port) do |socket|
-              socket.sync = true
-              socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
-              socket.each_line() do |line|
-                puts("<-\t%s" % line.chomp())
-                action_json = line.chomp()
-                action_obj = JSON.parse(action_json)
-                case action_obj["type"]
-                  when "hello"
-                    response_json = JSON.dump({
-                        "type" => "join",
-                        "name" => @params[:name],
-                        "room" => uri.path.slice(/^\/(.*)$/, 1),
-                    })
-                  when "error"
-                    break
-                  else
-                    if action_obj["type"] == "start_game"
-                      @my_id = action_obj["id"]
-                      self.players = Array.new(4) do |i|
-                        i == @my_id ? @params[:player] : PuppetPlayer.new()
-                      end
+          TCPSocket.open(uri.host, uri.port) do |socket|
+            socket.sync = true
+            socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+            socket.each_line() do |line|
+              puts("<-\t%s" % line.chomp())
+              action_json = line.chomp()
+              action_obj = JSON.parse(action_json)
+              case action_obj["type"]
+                when "hello"
+                  response_json = JSON.dump({
+                      "type" => "join",
+                      "name" => @params[:name],
+                      "room" => uri.path.slice(/^\/(.*)$/, 1),
+                  })
+                when "error"
+                  break
+                else
+                  if action_obj["type"] == "start_game"
+                    @my_id = action_obj["id"]
+                    self.players = Array.new(4) do |i|
+                      i == @my_id ? @params[:player] : PuppetPlayer.new()
                     end
-                    action = Action.from_json(action_json, self)
-                    responses = do_action(action)
-                    break if action.type == :end_game
-                    response = responses && responses[@my_id]
-                    response_json = response ? response.to_json() : JSON.dump({"type" => "none"})
-                end
-                puts("->\t%s" % response_json)
-                socket.puts(response_json)
+                  end
+                  action = Action.from_json(action_json, self)
+                  responses = do_action(action)
+                  break if action.type == :end_game
+                  response = responses && responses[@my_id]
+                  response_json = response ? response.to_json() : JSON.dump({"type" => "none"})
               end
+              puts("->\t%s" % response_json)
+              socket.puts(response_json)
             end
           end
-        rescue  #　例外処理
-          $stderr.puts("connection failed")
         end
 
         def expect_response_from?(player)
